b91a253748b77bc2fdf42f0e98cbddc276d0e509
Ivan <ivan-kostyulin@yandex.ru>
2025-04-23 11:35:32 +0300

    feat: compress media

--- src/pages/History/components/Footer/Footer.tsx
+++ src/pages/History/components/Footer/Footer.tsx
@@ -30,10 +30,10 @@ const Footer: FC<IFooterProps> = ({ windowWidth }) => {
           }
           <div className='h-footer__navigation'>
             <Link className='h-header__item' to='stages' smooth={true} offset={0} duration={1750} spy={true}>Этапы</Link>
-            <Link className='h-header__item' to='cases' smooth={true} offset={0} duration={1250} spy={true}>Кейсы</Link>
+            <Link className='h-header__item' to='cases' smooth={true} offset={-40} duration={1250} spy={true}>Кейсы</Link>
             <Link className='h-header__item' to='final' smooth={true} offset={0} duration={1000} spy={true}>Финал</Link>
-            <Link className='h-header__item' to='prize' smooth={true} offset={0} duration={750} spy={true}>Призы</Link>
-            <Link className='h-header__item' to='news' smooth={true} offset={0} duration={500} spy={true}>СМИ</Link>
+            <Link className='h-header__item' to='prize' smooth={true} offset={-40} duration={750} spy={true}>Призы</Link>
+            <Link className='h-header__item' to='news' smooth={true} offset={-40} duration={500} spy={true}>СМИ</Link>
           </div>
         </div>
         <p className='h-footer__copy'>&copy; {getCurrentYear()}, Все права защищены РУТ (МИИТ)</p>
--- src/pages/History/components/Header/Header.tsx
+++ src/pages/History/components/Header/Header.tsx
@@ -11,10 +11,10 @@ const Header = () => {
       <nav className='h-header__nav'>
         <div className='h-header__list'>
           <Link className='h-header__item' to='stages' smooth={true} offset={0} duration={500} spy={true}>Этапы</Link>
-          <Link className='h-header__item' to='cases' smooth={true} offset={0} duration={750} spy={true}>Кейсы</Link>
+          <Link className='h-header__item' to='cases' smooth={true} offset={-40} duration={750} spy={true}>Кейсы</Link>
           <Link className='h-header__item' to='final' smooth={true} offset={0} duration={1000} spy={true}>Финал</Link>
-          <Link className='h-header__item' to='prize' smooth={true} offset={0} duration={1250} spy={true}>Призы</Link>
-          <Link className='h-header__item' to='news' smooth={true} offset={0} duration={1750} spy={true}>СМИ</Link>
+          <Link className='h-header__item' to='prize' smooth={true} offset={-40} duration={1250} spy={true}>Призы</Link>
+          <Link className='h-header__item' to='news' smooth={true} offset={-40} duration={1750} spy={true}>СМИ</Link>
         </div>
       </nav>
       <a className='h-header__button' target='_blank' rel='norefferer' href='https://t.me/contestmiit'>Telegram-канал</a>
